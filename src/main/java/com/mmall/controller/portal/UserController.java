dfbb3164e67adb38894ba090a888960961c4f409
a5302091 <419454569@qq.com>
2018-07-28 12:43:06 +0800

    cook

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -8,6 +8,7 @@ import com.mmall.service.IUserService;
 import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPollUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,10 +31,10 @@ public class UserController {
     //用户登录
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse response, HttpServletRequest request) {
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse response) {
         ServerResponse<User> login = iUserService.login(username, password);
         if (login.isSuccess()) {
-            CookieUtil.writeLoginToken(response,session.getId());
+            CookieUtil.writeLoginToken(response, session.getId());
             RedisPollUtil.setEx(session.getId(), JsonUtil.obj2String(login.getData()), Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
         return login;
@@ -49,9 +50,10 @@ public class UserController {
     //用户退出
     @RequestMapping(value = "logout.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> logout(HttpSession session,HttpServletRequest request,HttpServletResponse response) {
-        session.removeAttribute(Const.CURRENT_USER);
+    public ServerResponse<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        String loginToken = CookieUtil.readLoginToken(request);
         CookieUtil.delLoginToken(request, response);
+        RedisPollUtil.del(loginToken);
         return ServerResponse.createBySuccess();
     }
 
@@ -65,8 +67,14 @@ public class UserController {
     //获取登录用户信息
     @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
+    public ServerResponse<User> getUserInfo(HttpServletRequest request) {
+        String loginToken = CookieUtil.readLoginToken(request);
+        if (StringUtils.isEmpty(loginToken)) {
+            return ServerResponse.createByErrorMessage("用户未登录,无法获取用户信息");
+        }
+        String userJsonStr = RedisPollUtil.get(loginToken);
+        User user = JsonUtil.string2Obj(userJsonStr, User.class);
+
         if (user != null) {
             return ServerResponse.createBySuccess(user);
         }
