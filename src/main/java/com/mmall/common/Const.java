8183d25490373f5d0247fd9c138b79b66349891b
a5302091 <419454569@qq.com>
2018-07-31 09:00:17 +0800

    dandian

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -13,8 +13,6 @@ public class Const {
 
     public static final String TOKEN_PREFIX = "token_";
 
-    public static final String a="测试";
-
     public interface RedisCacheExtime{
         int REDIS_SESSION_EXTIME=60*30;
     }
