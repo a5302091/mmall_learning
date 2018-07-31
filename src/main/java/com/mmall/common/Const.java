3ac3de875c4b57edd3a2a3a2537ff0bb4d70d557
a5302091 <419454569@qq.com>
2018-07-31 09:19:03 +0800

    cv

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -12,6 +12,7 @@ public class Const {
     public static final String USERNAME = "username";
 
     public static final String TOKEN_PREFIX = "token_";
+    public static final String aa = "ddad";
 
     public interface RedisCacheExtime{
         int REDIS_SESSION_EXTIME=60*30;
