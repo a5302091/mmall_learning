7ab43625537cb6d224b79606e879a719d119a6ff
a5302091 <419454569@qq.com>
2018-07-31 08:54:40 +0800

    cs

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -13,6 +13,8 @@ public class Const {
 
     public static final String TOKEN_PREFIX = "token_";
 
+    public static final String a="测试";
+
     public interface RedisCacheExtime{
         int REDIS_SESSION_EXTIME=60*30;
     }
